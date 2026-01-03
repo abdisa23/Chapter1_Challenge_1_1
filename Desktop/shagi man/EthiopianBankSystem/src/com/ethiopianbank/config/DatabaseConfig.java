84827334bf9a18cfbc566cdd6e6bdef39ce9c63d
abdisa23 <abdisaosman1@gmail.com>
2026-01-03 23:53:10 +0300

    Delete Desktop directory

--- Desktop/shagi man/EthiopianBankSystem/src/com/ethiopianbank/config/DatabaseConfig.java
+++
@@ -1,8 +0,0 @@
-package com.ethiopianbank.config;
-
-public class DatabaseConfig {
-    public static final String URL = "jdbc:mysql://localhost:3306/ethiopian_bank";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "hafi4642";
-    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-}
